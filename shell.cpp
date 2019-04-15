b1f1cb60f4d8712cbea69a93987fdd3b9d1202ac
Robert Unverzagt <robert.unverzagt@gmail.com>
2019-04-14 20:08:17 -0600

    realized I was approaching some design from a C mindset. I have tried to recrify this with C++. please tag anywhere that I have gone wrong

--- shell.cpp
+++ shell.cpp
@@ -2,7 +2,7 @@
 
 class Command
 {
-    char * program;
+    char * path;
     char * inputArray[MAX_ARG_SIZE];
     char inputStr[MAX_BUF_SIZE];
     int arguments;
@@ -11,7 +11,7 @@ class Command
     public:
     Command(void)
     {
-        program = NULL;
+        path = NULL;
         for( int i = 0; i < MAX_ARG_SIZE; i++ )
         {
             inputArray[i] = NULL;
@@ -20,19 +20,19 @@ class Command
         arguments = 0;
     }
 
-    void setInput( const char * inStr )
+    void setInput( string inStr )
     {
-        // Parses inStr and populates inputArray and program accordingly
+        // Parses inStr and populates inputArray and path accordingly
         char * end;
         int i = 1;
-        strcpy( inputStr, inStr );
+        strcpy( inputStr, inStr.c_str() );
 
-        if( *inStr == '\n' )
+        if( *inputStr == '\n' )
             // No input, just return
             return;
 
         inputArray[0] = inputStr;
-        program = inputStr;
+        path = inputStr;
         arguments = 1;
         end = inputStr;
 
@@ -71,30 +71,31 @@ class Command
     void execute(void)
     {
         // Execute the command in inputArray
-        if( program == NULL )
-            // don't do anything if no program is given
+        if( path == NULL )
+            // don't do anything if no path is given
             // since we are in the child process, kill yourself
             exit(0);
 
-        execv( program, inputArray );
+        execv( path, inputArray );
     }
 };
 
 class Shell
 {
-    char strBuf[MAX_BUF_SIZE];
+    string strBuf;
+    //char strBuf[MAX_BUF_SIZE];
     Command currentCmd;
 
     public:
     void getInput( void )
     {
         // Gets an input from the user, and creates a command based on it
-        printf( "$>" );
-        fgets( strBuf, MAX_BUF_SIZE, stdin );
+        cout << "$>";
+        getline( cin, strBuf );
         moveInputToCommand( strBuf );
     }
 
-    void moveInputToCommand( char * inString )
+    void moveInputToCommand( string inString )
     {
         // Creates a new command object based on the input
         currentCmd.setInput( inString );
--- shell.h
+++ shell.h
@@ -1,9 +1,11 @@
-#include <sys/types.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+//#include <cstdlib>
 #include <unistd.h>
-#include <errno.h>
-#include <string.h>
+//#include <errno>
+#include <string>
+#include <string.h> // need strcpy from this
+
+using namespace std;
 
 #define MAX_BUF_SIZE 1024
 #define MAX_ARG_SIZE 128
